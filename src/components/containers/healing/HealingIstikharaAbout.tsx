f2731ac3ed17a7579f63153b32b474242c24235e
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-04-29 15:23:17 +0500

    Healing Page

---
+++ src/components/containers/healing/HealingIstikharaAbout.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Image from "next/image";
+import HealingIstikhara from "public/images/istikhara-qalandar-istikhara-for-healing.jpg";
+import star from "public/images/star.png";
+import dotlarge from "public/images/agency/dot-large.png";
+
+const HealingIstikharaAbout = () => {
+  return (
+    <section className="section award">
+      <div className="container">
+        <div className="row gaper align-items-center">
+          <div className="col-12 col-lg-6">
+            <div className="award__thumb dir-rtl">
+              <Image
+                src={HealingIstikhara}
+                alt="istikhara-qalandar-istikhara-for-healing"
+                className="unset fade-left"
+              />
+            </div>
+          </div>
+          <div className="col-12 col-lg-6 col-xxl-5 offset-xxl-1">
+            <div className="award__content section__content">
+              <span className="sub-title">
+                Istikhara For Healing{" "}
+                <i className="fa-solid fa-arrow-right"></i>
+              </span>
+              <h2 className="title title-anim">
+                Finding Comfort: <span>Istikhara for Healing</span>
+              </h2>
+              <div className="paragraph">
+                <p>
+                  Embark on a deep spiritual healing and guidance journey with
+                  Istikhara. Istikhara provides a sacred path to seek divine aid
+                  and clarity amid spiritual challenges. Our compassionate team
+                  is here to assist you through every stage of the Istikhara
+                  process. With Istikhara, you can forge a profound connection
+                  with your inner self and discover serenity amidst life's
+                  spiritual intricacies. Begin your Istikhara journey today and
+                  witness the transformative influence of divine guidance in
+                  surmounting spiritual hurdles. Let Istikhara illuminate your
+                  way as you navigate your spiritual journey with clarity and
+                  elegance.{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Image
+        src={star}
+        alt="istikhara-qalandar-istikhara-for-healing-star"
+        className="star"
+      />
+      <Image
+        src={star}
+        alt="istikhara-qalandar-istikhara-for-healing-star"
+        className="star-two"
+      />
+      <Image
+        src={dotlarge}
+        alt="istikhara-qalandar-istikhara-for-healing-dots-large"
+        className="dot"
+      />
+      <Image
+        src={dotlarge}
+        alt="istikhara-qalandar-istikhara-for-healing-large"
+        className="dot-two"
+      />
+    </section>
+  );
+};
+
+export default HealingIstikharaAbout;
---
+++ src/pages/healing.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Layout from "@/components/layout/Layout";
+import CmnBanner from "@/components/layout/banner/CmnBanner";
+import SEO from "@/components/seo";
+import CallToActionThree from "@/components/containers/common/CallToActionThree";
+import HealingIstikharaAbout from "@/components/containers/healing/HealingIstikharaAbout";
+
+const OurStory = () => {
+  return (
+    <Layout header={2} footer={1}>
+      <SEO pageTitle="Istikhara For Healing" />
+      <CmnBanner
+        title="Istikhara For Healing"
+        navigation="Istikhara For Healing"
+      />
+      <HealingIstikharaAbout />
+      <CallToActionThree />
+    </Layout>
+  );
+};
+
+export default OurStory;
